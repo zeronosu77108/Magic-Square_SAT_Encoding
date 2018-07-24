29675a2a65ef6b034e70ffc310a9c9ff6489b950
zeronosu77108 <taiga77108@gmail.com>
2018-07-25 08:39:50 +0900

    Add function define_variables

--- Direct_Encoding.rb
+++ Direct_Encoding.rb
@@ -1,5 +1,20 @@
 #!/usr/bin/env ruby
 
+def define_variables(n,file)
+    1.upto(n**2) do |i|
+        1.upto(n**2) do |j|
+            file.print "p(x_#{i}=#{j}) "
+        end
+
+        1.upto(n**2) do |j|
+            (j+1).upto(n**2) do |k|
+                file.puts "¬p(x_#{i}=#{j}) ¬p(x_#{i}=#{k})"
+            end
+        end
+
+    end
+end
+
 
 def main()
     print "N="
@@ -7,9 +22,15 @@ def main()
     print "M="
     m = gets.to_i
     puts "Encoded file name"
-    en_file_name = gets
+    en_file_name = gets.chomp
     puts "Result file name"
-    reslut_file_name = gets
+    reslut_file_name = gets.chomp
+
+    en_file = File.open(en_file_name,"w")
+
+    define_variables(n,en_file)
+
+    en_file.close()
 end
 
 main()
