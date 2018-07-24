34feddd2705243c2a8eb944de577aa545e4e39d9
zeronosu77108 <taiga77108@gmail.com>
2018-07-25 04:28:53 +0900

    Add main()

---
+++ Direct_Encoding.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+
+def main()
+    print "N="
+    n = gets.to_i
+    print "M="
+    m = gets.to_i
+    puts "Encoded file name"
+    en_file_name = gets
+    puts "Result file name"
+    reslut_file_name = gets
+end
+
+main()
